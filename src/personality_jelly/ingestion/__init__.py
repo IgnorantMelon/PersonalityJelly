1fe1b7c46abaf97ba9875a8b094d13a4c6b1692f
Ema <abcdefgg@126.com>
2026-05-19 13:34:39 +0800

    Add source ingestion service

--- src/personality_jelly/ingestion/__init__.py
+++ src/personality_jelly/ingestion/__init__.py
@@ -2,6 +2,19 @@
 
 from personality_jelly.ingestion.chunker import ChunkingConfig, chunk_source_text
 from personality_jelly.ingestion.loader import LoadedSource, load_text_source
+from personality_jelly.ingestion.service import (
+    SourceIngestionResult,
+    ingest_loaded_source,
+    ingest_text_file,
+)
 
-__all__ = ["ChunkingConfig", "LoadedSource", "chunk_source_text", "load_text_source"]
+__all__ = [
+    "ChunkingConfig",
+    "LoadedSource",
+    "SourceIngestionResult",
+    "chunk_source_text",
+    "ingest_loaded_source",
+    "ingest_text_file",
+    "load_text_source",
+]
 
